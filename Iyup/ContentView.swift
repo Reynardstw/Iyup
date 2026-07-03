333ab64a75f50580bbfdc6bcdece238826bbd6f9
reynard <reynardsetiawan.rs@gmail.com>
2026-07-03 14:08:13 +0700

    Initial Commit

---
+++ Iyup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Iyup
+//
+//  Created by Reynard Setiawan on 03/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Iyup/IyupApp.swift
@@ -0,0 +1,17 @@
+//
+//  IyupApp.swift
+//  Iyup
+//
+//  Created by Reynard Setiawan on 03/07/26.
+//
+
+import SwiftUI
+
+@main
+struct IyupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
